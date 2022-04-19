b77056f3a9c18d1551536fcea48d93dfbbc97964
Saloni Fernandes <saloni.ferdz@gmail.com>
2022-04-19 12:36:37 +0530

    required changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,1 @@
-import Buttons from "./components/button.component"
 
-function App() {
-  return (
-    <div>
-      <Buttons text="Login" click={()=>alert("hello")} variant="contained"/>
-      <Buttons text="Check Now" click={()=>alert("hello")} variant="contained"/>
-      <Buttons text="Browse" click={()=>alert("hello")} variant="outlined"/>
-      <Buttons text="Submit" click={()=>alert("hello")} variant="contained"/>
-    </div>
-  );
-}
-export default App;
--- src/components/button.component.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-
-function Buttons({click,text,variant}) {
-  return (<Button {variant}onClick={() => {click()}} style={{  backgroundColor:"#00D09C" }}>{text}</Button>);
-}
-
-export default Buttons;
